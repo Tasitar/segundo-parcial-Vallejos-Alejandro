23f56dc13b99e5e56276b3120cd8af9a0911cddb
alejandroiv <alejandroiv177771@gmail.com>
2025-06-27 12:47:46 -0300

    intentando levantar el server

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 import { express } from "express";
+import { Sequelize } from "sequelize";
 
-
+configDotenv.config()
 app.use(express.json())
+
+try{
+    await Sequelize.autenticate();
+    console.log('hola soy el server estoy conectado')
+}catch(error){
+    console.log('hola soy el server y no hay un error al conectarme')
+}
+
+const PORT = process.env.PORT
+app.listen(PORT,()=>{
+    console.log ('hola soy el server y estoy corriendo')
+})
